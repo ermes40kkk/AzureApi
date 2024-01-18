92b9f3c4c341c6a92f42ccb170a4c41ddbffae00
ermes40kkk <61497437+ermes40kkk@users.noreply.github.com>
2024-01-18 14:48:56 +0100

    one

---
+++ src/main/java/mossino/azure/apiAzure/ApiAzureApplication.java
@@ -0,0 +1,13 @@
+package mossino.azure.apiAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mossino/azure/apiAzure/ApiAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package mossino.azure.apiAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
